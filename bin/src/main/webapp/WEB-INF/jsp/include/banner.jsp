b50ad67d66ce8eda8911b0af8efbaa715a416c3f
gengstah <gerardpdelasarmas@gmail.com>
2013-03-14 19:52:38 +0800

    Removed bin folder

--- bin/src/main/webapp/WEB-INF/jsp/include/banner.jsp
+++
@@ -1,3 +0,0 @@
-<div align="center">
-	<img src="<c:url value="/css/images/banner.jpg" />" alt="<fmt:message key="companyNameHeader" />" />
-</div>
--- bin/src/main/webapp/js/companyform.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$("input:button#addCompanyButton").button().click(function() {
-		$('form#company').submit();
-	});
-	$("input:button#cancelCompanyButton").button();
-	$("input:button#addAnotherButton").button();
-	$("input:button#companyListButton").button();
-});
